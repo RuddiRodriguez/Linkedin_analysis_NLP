c635066e95781fd141a368330bae2e7976ad9ea0
Ruddi Rodriguez Garcia <rudiscu@gmail.com>
2019-08-26 01:34:33 +0200

    added pandas pipeline_FeatureSelector
    Tested pandas pipe and first transformer FeatureSelector
    does work

--- Linkedin_pipelines.py
+++ Linkedin_pipelines.py
@@ -5,9 +5,35 @@ Created on Mon Aug 26 00:53:08 2019
 
 @author: ruddirodriguez
 """
+import pandas as pd 
+from module_transformers import FeatureSelector
+# Create a function that
+def drop_nan(df, col):
+    # drop nan values
+    return df[df[col] != 'Not Applicable']
 
 # Create a function that
-def drop_nan(dataframe, col):
-    # groups the data by a column and returns the mean age per group
-    return dataframe = dataframe[data.col != 'Not Applicable']
+def data_categorization(df, col):
+    df['Cat_level'] = df[col]
+    labels = df[col].astype('category').cat.categories.tolist()
+    replace_map_comp = {col: {k: v for k, v in zip(
+        labels, list(range(1, len(labels)+1)))}}
+    df.replace(replace_map_comp, inplace=True)
+    #print(replace_map_comp)
+
+    return df
+
+
+training_data_name = 'demodata_training_full_v1.csv'
+training_data = pd.read_csv('Data/'+training_data_name)
+# Create a pipeline that applies the mean_age_by_group function
+cleaned_data = (training_data.pipe(drop_nan, col='Level')
+   # then applies the uppercase column name function
+   .pipe(data_categorization, col='Level')
+)
+
+test = FeatureSelector ('Level')
+dd = test.transform (cleaned_data)
+
+
 
