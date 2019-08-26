20a5ca23c5da4745ae07ca0108e5def538f23c25
Ruddi Rodriguez Garcia <rudiscu@gmail.com>
2019-08-26 03:02:35 +0200

    Full pipeline working_version

--- Linkedin_pipelines.py
+++ Linkedin_pipelines.py
@@ -6,7 +6,12 @@ Created on Mon Aug 26 00:53:08 2019
 @author: ruddirodriguez
 """
 import pandas as pd 
-from module_transformers import FeatureSelector
+from module_transformers import FeatureSelector, cleaning_text_regular_exp, removing_stop_words
+from sklearn.pipeline import FeatureUnion, Pipeline 
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from module_print import print_summary
 # Create a function that
 def drop_nan(df, col):
     # drop nan values
@@ -32,8 +37,41 @@ cleaned_data = (training_data.pipe(drop_nan, col='Level')
    .pipe(data_categorization, col='Level')
 )
 
-test = FeatureSelector ('Level')
-dd = test.transform (cleaned_data)
 
 
+#testd = cleaning_text_regular_exp ('Description')
+#dd = testd.transform (cleaned_data)
+#
+#
+#testr = removing_stop_words ('Description')
+#ddd = testr.transform (dd)
+#
+#test = FeatureSelector ('Description')
+#dddd = test.transform (ddd)
 
+full_pipeline = Pipeline( steps = [ ( 'pre_regular_exp', cleaning_text_regular_exp ('Description') ),
+                                    ( 'pre_stop_words', removing_stop_words ('Description')), 
+                                    ( 'Pre_selector', FeatureSelector ('Description')),
+                                    ( 'vectorized', CountVectorizer()),] )
+    
+    
+#Leave it as a dataframe becuase our pipeline is called on a 
+#pandas dataframe to extract the appropriate columns, remember?
+X = cleaned_data.drop('Level',axis=1)
+#You can covert the target variable to numpy 
+y = cleaned_data['Level'].values 
+
+X_train, X_test, y_train, y_test = train_test_split( X, y , test_size = 0.2 , random_state = 42 )
+
+#The full pipeline as a step in another pipeline with an estimator as the final step
+full_pipeline_m = Pipeline( steps = [ ( 'full_pipeline', full_pipeline),
+                                  
+                                  ( 'model', LogisticRegression() ) ] )
+
+#Can call fit on it just like any other pipeline
+full_pipeline_m.fit( X_train, y_train )  
+#Can predict with it like any other pipeline
+y_pred = full_pipeline_m.predict( X_test ) 
+print_summary(y_test, y_pred,
+                  cleaned_data, "Cat_level")
+  
--- module_transformers.py
+++ module_transformers.py
@@ -12,12 +12,14 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import FeatureUnion, Pipeline 
+import nltk
+from nltk.corpus import stopwords
 
 #Custom Transformer that extracts columns passed as argument to its constructor 
 class FeatureSelector( BaseEstimator, TransformerMixin ):
     #Class Constructor 
     def __init__( self, feature_names ):
-        self._feature_names = feature_names 
+        self.feature_names = feature_names 
     
     #Return self nothing else to do here    
     def fit( self, X, y = None ):
@@ -25,4 +27,47 @@ class FeatureSelector( BaseEstimator, TransformerMixin ):
     
     #Method that describes what we need this transformer to do
     def transform( self, X, y = None ):
-        return X[ self._feature_names ] 
+        return X[self.feature_names].tolist()#np.asarray(X[self.feature_names]).astype(str)#np.c_[X]#X[self.feature_names]#pd.DataFrame (X,columns= self.feature_names) 
+    
+    
+class cleaning_text_regular_exp(BaseEstimator, TransformerMixin):
+     #Class Constructor 
+    def __init__( self, col_name ):
+        self.col_name = col_name 
+    
+    #Return self nothing else to do here    
+    def fit( self, X, y = None ):
+        return self 
+    
+    #Method that describes what we need this transformer to do
+    def transform( self, X, y = None ):
+        #df[text_field] = df[text_field].str.replace(r"http\S+", "")
+        X[self.col_name] = X[self.col_name].str.replace(r'[^\w\s]', " ")
+        X[self.col_name] = X[self.col_name].str.replace(r":", "")
+        X[self.col_name] = X[self.col_name].str.replace(
+                r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " ")
+        X[self.col_name] = X[self.col_name].str.replace(r"\d", " ")
+        X[self.col_name] = X[self.col_name].str.lower()
+    
+        return X
+    
+class removing_stop_words(BaseEstimator, TransformerMixin):
+            #Class Constructor 
+         def __init__( self, col_name ):
+             self.col_name = col_name 
+    
+            #Return self nothing else to do here    
+         def fit( self, X, y = None ):
+                return self 
+    
+    #Method that describes what we need this transformer to do
+         def transform( self, X, y = None ):
+            nltk.download("stopwords")
+            stop = stopwords.words('english')
+            X[self.col_name]=X[self.col_name].apply(
+                    lambda x: " ".join([item for item in x.split() if item not in stop]))
+            return X
+    
+        
+
+    
